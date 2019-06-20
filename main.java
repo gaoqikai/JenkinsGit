5c9949576647e64fb1efc8937aed27901ec3a6e6
Name <gqk9000@outlook.com>
2019-06-20 17:34:27 +0800

    2nd

--- main.java
+++ main.java
@@ -1,5 +1,7 @@
-public class main{
-    public static void main (String[] args){
+public class main {
+    public static void main(String[] args) {
         System.out.println("This is a sample test");
+        int a = 10, b = 10;
+        System.out.println(a * b);
     }
 }
