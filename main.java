306a9a86be8ad2452f35538cf89ef5a0b953b933
Name <gqk9000@outlook.com>
2019-06-24 10:59:41 +0800

    6

--- main.java
+++ main.java
@@ -3,7 +3,7 @@ import java.util.TimeZone;
 public class main {
     public static void main(String[] args) {
         System.out.println("This is a sample test");
-        int a = 10, b = 8;
+        int a = 20, b = 8;
         System.out.println(a * b);
         for (String string : TimeZone.getAvailableIDs(TimeZone.getTimeZone("GMT+08:00").getRawOffset())) {
             System.out.println(string);
